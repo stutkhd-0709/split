40fcb974b7d8eb877a52df99c606a3e5a56a2b08
Takahide Sato <sato_t@cyberowl.co.jp>
2023-08-16 01:31:03 +0900

    add dist option

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -2,6 +2,8 @@ package helpers
 
 import (
 	"fmt"
+	"os"
+	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -52,12 +54,63 @@ func ConvertFileSizeToInt(strFileSize string) (int, error) {
 	return intFileByteSize, nil
 }
 
-func GenerateFilename(prefix string, count int, extension string) string {
+func GenerateFilename(dist string, count int, extension string) (string, error) {
 	// rune型として扱う -> 文字コードのこと
 	// 元の文字に戻すにはstring関数を使う
-	firstChar := 'a' + (count % 26)
-	secondChar := 'a' + (count / 26 % 26)
+	firstChar := 'a' + (count / 26 % 26)
+	secondChar := 'a' + (count % 26)
+
+	var prefix string
+	if dist == "" {
+		prefix = "x"
+	} else {
+		isDir, err := ensureDirectoryExists(dist)
+		if err != nil {
+			return "", err
+		}
+
+		if isDir {
+			prefix = dist + "x"
+		} else {
+			prefix = dist
+		}
+	}
 
 	// %cでUnicodeを表す
-	return fmt.Sprintf("%s%c%c%s", prefix, firstChar, secondChar, extension)
+	return fmt.Sprintf("%s%c%c%s", prefix, firstChar, secondChar, extension), nil
+}
+
+func ParseFilePath(filepath string) (string, string) {
+	inputFilename := path.Base(filepath)
+	inputFileExt := path.Ext(filepath)
+	inputFileWithoutExt := inputFilename[:len(inputFilename)-len(inputFileExt)]
+
+	return inputFileWithoutExt, inputFileExt
+}
+
+func ensureDirectoryExists(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		// pathが存在しない場合
+		if os.IsNotExist(err) {
+			// ディレクトリかどうか
+			if strings.HasSuffix(path, "/") {
+				// 新規作成
+				err := os.MkdirAll(path, 0755)
+				if err != nil {
+					return true, err
+				}
+				return true, nil
+			} else {
+				return false, nil
+			}
+		} else {
+			return false, err
+		}
+	} else if !fileInfo.IsDir() {
+		// pathが存在して、そのpathがディレクトリでない場合
+		return false, nil
+	}
+	// すでにpathが存在している場合
+	return true, nil
 }
